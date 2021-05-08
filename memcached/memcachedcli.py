ab9b69053dbe5d80e4668af02d901b96ff15ebcb
Yiwei Li <leepoly2@gmail.com>
2021-05-08 16:02:00 +0800

    Add validate option

--- memcached/memcachedcli.py
+++ memcached/memcachedcli.py
@@ -2,12 +2,11 @@ import argparse
 import os, sys
 from pymemcache.client.base import Client
 
-hasValidation = True
 validDic = {}
 
 op_type = ['INSERT', 'READ', 'UPDATE', 'SCAN']
 
-def parse_line(line, client):
+def parse_line(line, client, hasValidation):
     found_op = 'None'
     for op in op_type:
         if line.startswith(op):
@@ -31,31 +30,32 @@ def parse_line(line, client):
     # print(op, ':', key, value)
 
 # YCSB load section
-def ycsb_load(load_trace, client):
+def ycsb_load(load_trace, client, hasValidation):
     insert_cnt = 0
     with open(load_trace) as load_file:
         for line in load_file:
-            parse_line(line, client)
+            parse_line(line, client, hasValidation)
             insert_cnt += 1
     print("insert {} entries".format(insert_cnt))
 
-def ycsb_run(run_trace, client):
+def ycsb_run(run_trace, client, hasValidation):
     op_cnt = 0
     with open(run_trace) as run_file:
         for line in run_file:
-            parse_line(line, client)
+            parse_line(line, client, hasValidation)
             op_cnt += 1
     print("execute {} entries".format(op_cnt))
 
 def main(args):
     port = args.port
     tracename = args.trace
+    hasValidation = args.validate
 
     server_addr = 'localhost:' + port
-    client = Client(server_addr)
+    client = Client(server_addr, default_noreply=True)
 
-    ycsb_load(tracename + '.load', client)
-    ycsb_run(tracename + '.run', client)
+    ycsb_load(tracename + '.load', client, hasValidation)
+    ycsb_run(tracename + '.run', client, hasValidation)
 
 
 def argparser():
@@ -67,6 +67,9 @@ def argparser():
     ap.add_argument('--trace', required=False,
                     default='workload/workloada',
                     help='Trace name. Make sure the existance of workloada.load and workloada.run respectively.')
+    ap.add_argument('--validate', action='store_true',
+                    help='Validate the correctness of memcached.')
+
     return ap
 
 if __name__ == '__main__':
